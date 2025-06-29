f7e77713f65da9455f5c19bcb888e8913dc749b2
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2025-06-29 17:11:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tap.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'your_secret_key'
+app.secret_key = 'supersecretkey'
 
 db = SQLAlchemy(app)
 
+# Модель пользователя
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -16,28 +17,29 @@ class User(db.Model):
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
 
+# Инициализация базы данных при старте
 with app.app_context():
     db.create_all()
-
-    # Создаем Enot, если нет
     if not User.query.filter_by(username='Enot').first():
-        admin = User(username='Enot', password='admin123', clicks=0)
+        admin = User(username='Enot', password='admin123')
         db.session.add(admin)
         db.session.commit()
 
+# Страница регистрации
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         if User.query.filter_by(username=username).first():
-            return "Пользователь существует"
+            return "Пользователь уже существует"
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('signin'))
     return render_template('signup.html')
 
+# Страница входа
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
@@ -50,18 +52,23 @@ def signin():
         return "Неверные данные"
     return render_template('signin.html')
 
+# Выход
 @app.route('/signout')
 def signout():
     session.pop('user_id', None)
     return redirect(url_for('signin'))
 
+# Главная страница с кнопкой
 @app.route('/')
 def index():
     if 'user_id' not in session:
         return redirect(url_for('signin'))
     user = User.query.get(session['user_id'])
-    return render_template('index.html', user=user)
+    all_users = User.query.order_by(User.clicks.desc()).all()
+    place = [i + 1 for i, u in enumerate(all_users) if u.id == user.id][0]
+    return render_template('index.html', user=user, place=place, users=all_users[:10])
 
+# Обработка нажатия
 @app.route('/tap', methods=['POST'])
 def tap():
     if 'user_id' not in session:
@@ -69,11 +76,22 @@ def tap():
     user = User.query.get(session['user_id'])
     user.clicks += 1
     db.session.commit()
-    win = False
-    if user.clicks % 10000 == 0:
-        win = True
-    return {'clicks': user.clicks, 'win': win}
+    win = user.clicks % 10000 == 0
+    return jsonify({'clicks': user.clicks, 'win': win})
 
+# Отправка координат
+@app.route('/location', methods=['POST'])
+def location():
+    if 'user_id' not in session:
+        return "Unauthorized", 401
+    data = request.get_json()
+    user = User.query.get(session['user_id'])
+    user.latitude = data.get('lat')
+    user.longitude = data.get('lon')
+    db.session.commit()
+    return '', 204
+
+# Карта с координатами (только для Enot)
 @app.route('/locations')
 def locations():
     if 'user_id' not in session:
@@ -81,7 +99,7 @@ def locations():
     user = User.query.get(session['user_id'])
     if user.username != 'Enot':
         return "Доступ запрещён", 403
-    users = User.query.filter(User.latitude != None, User.longitude != None).all()
+    users = User.query.filter(User.latitude.isnot(None), User.longitude.isnot(None)).all()
     return render_template('map.html', users=users)
 
 if __name__ == '__main__':
